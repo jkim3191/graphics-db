0d394c867d44be040f51648b7787f5b2a209bdfc
Yunho <yhjc2692@gmail.com>
2025-08-16 17:57:16 -0400

    Misc: initialize logging @ `main.py`

--- src/graphics_db_server/main.py
+++ src/graphics_db_server/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from graphics_db_server.api.v0.endpoints import assets
+from graphics_db_server.logging import configure_logging
+
+configure_logging()
 
 app = FastAPI(title="Graphics DB API")
-
 app.include_router(assets.router, prefix="/api/v0", tags=["Assets"])
 
 
