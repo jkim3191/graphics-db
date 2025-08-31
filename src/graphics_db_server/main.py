a3f4c03de6e8039659ad41e4a888a2c9bacb620c
Yunho <yhjc2692@gmail.com>
2025-08-31 16:07:30 -0400

    Added CORS configuration

--- src/graphics_db_server/main.py
+++ src/graphics_db_server/main.py
@@ -1,5 +1,6 @@
 import psycopg
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from graphics_db_server.api.v0.endpoints import assets
 from graphics_db_server.core.config import TABLE_NAME
@@ -9,6 +10,15 @@ from graphics_db_server.logging import configure_logging, logger
 configure_logging(level="DEBUG")
 
 app = FastAPI(title="Graphics DB API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(assets.router, prefix="/api/v0", tags=["Assets"])
 
 
